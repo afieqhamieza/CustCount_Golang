b0c665fc7b83915b7cf1ccc3f033cf8191217892
afieqhamieza <afieqhaazemi@gmail.com>
2021-06-20 01:53:51 -0400

    passing data to template

--- main.go
+++ main.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
-
+	// "fmt"
+	// "html/template"
 	"github.com/thedevsaddam/renderer"
 )
 
 var rnd *renderer.Render
+
+
+type Info struct{
+	storeName string;
+	count int;
+	capacity int;
+}
+
+var info Info
 
 func init() {
 	opts := renderer.Options{
@@ -15,6 +25,9 @@ func init() {
 	}
 
 	rnd = renderer.New(opts)
+	info.storeName = "My groceries store"
+	info.count = 0
+	info.capacity = 200
 }
 
 func main() {
@@ -22,7 +35,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/counter", counter)
 	mux.HandleFunc("/settings", settings)
-	port := ":9000"
+	port := ":8080"
 	log.Println("Listening on port ", port)
 	http.ListenAndServe(port, mux)
 }
@@ -32,7 +45,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
-	rnd.HTML(w, http.StatusOK, "counter", nil)
+	data := map[string]interface{}{
+        "storeName": info.storeName,
+		"count": info.count,
+		"capacity": info.capacity,
+    }
+	rnd.HTML(w, http.StatusOK, "counter", data)
+	
 }
 
 func settings(w http.ResponseWriter, r *http.Request) {
